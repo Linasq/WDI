f94dc8db9fdc7623c46a3cbfdd416aa64f5ea0dd
Linasq <linasmg58@gmail.com>
2022-11-06 20:43:33 +0100

    dodano przypadki testowe

--- laboratorium_02/cz_02_zadanie_10.py
+++ laboratorium_02/cz_02_zadanie_10.py
@@ -7,19 +7,22 @@ sum_prime([3, 5, 7, 9, 11, 13]) ➞ "(3 12)(5 5)(7 7)(11 11)(13 13)"
 def sum_prime(tab):
     dict={}
     for i in tab:
-        n=i
-        x = 2
-        while n!=1:
-            if n%x==0:
-                if x in dict.keys():
-                    dict[x]+=i
-                else:
-                    dict.update({x:i})
-                while n%x==0:
-                    n//=x
-            x+=1
-    print(dict)
+        if i>0:
+            n=i
+            x = 2
+            while n!=1:
+                if n%x==0:
+                    if x in dict.keys():
+                        dict[x]+=i
+                    else:
+                        dict.update({x:i})
+                    while n%x==0:
+                        n//=x
+                x+=1
+    return dict
 
-sum_prime([7,13,18,23,24])
-sum_prime([3, 5, 7, 9, 11, 13])
-sum_prime([121, 100, 33, 66, 12, 450, 123])
+print(f'1: {sum_prime([7,13,18,23,24])}')
+print(f'2: {sum_prime([3, 5, 7, 9, 11, 13])}')
+print(f'3: {sum_prime([121, 100, 33, 66, 12, 450, 123])}')
+print(f'4: {sum_prime([1,1,1,11,1,11,0,0,0,0,1,1,1,11,11])}')
+print(f'5: {sum_prime([-7,-13,-18,-23,24])}')
